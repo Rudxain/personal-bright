6cb3b4b772851b624530317d145afbde1bc47c39
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-09-15 03:19:00 -0400

    `max` instead of `mean`, refactor, format, better docs, no pipfiles

--- src/main.py
+++ src/main.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
 from typing import Final
-import sys
-import time
-import screen_brightness_control as sbc
-import numpy as np
+from sys import argv
+from time import sleep as zz
+from screen_brightness_control import fade_brightness
+from numpy import ndarray as nda
 import cv2
 
-def u8_to_percent(b: int) -> float:
+
+def u8_to_percent(b: int):
+	'''convert a byte into its corresponding ratio, then scale by a factor of a hundred'''
 	return b / 0xff * 100
 
-def mean_light(img: np.ndarray) -> float:
-	return sum(cv2.mean(img)) / 3
 
-def take_photo() -> np.ndarray:
+def take_lil_photo() -> nda:
+	'''capture 1 frame from main cam with minimum resolution'''
 	cam = cv2.VideoCapture(0)
 
-	#set resolution to the min possible (varies with device)
-	#for general perfomance (time, energy, and memory)
+	# set resolution to the min possible (varies with device)
+	# for general perfomance (time, energy, and memory)
 	cam.set(cv2.CAP_PROP_FRAME_WIDTH, 1)
 	cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 1)
 
@@ -24,8 +25,8 @@ def take_photo() -> np.ndarray:
 	cam.release()
 	return img
 
-#this increases versatility by taking args from either CLI or script
-def main(*args: str):
+
+def main(*args: str):  # this increases versatility by taking args from either CLI or script
 	DEFAULT_T: Final = 3.0
 
 	HELP_TXT: Final = f'usage: {args[0]} [period={DEFAULT_T}]\n`period` is the sample-interval in seconds'
@@ -40,13 +41,15 @@ def main(*args: str):
 
 	ambient_old = None
 	while True:
-		ambient_new = round(mean_light(take_photo()))
+		ambient_new = take_lil_photo().max()
 
-		#save energy and time
+		# save energy and time
 		if ambient_old != ambient_new:
 			ambient_old = ambient_new
-			sbc.fade_brightness(finish=light_map[ambient_new], blocking=True, display=0)
-		time.sleep(period)
+			fade_brightness(
+				finish=light_map[ambient_new], blocking=True, display=0)
+		zz(period)
 
-if __name__=='__main__':
-	main(*sys.argv)
+
+if __name__ == '__main__':
+	main(*argv)
